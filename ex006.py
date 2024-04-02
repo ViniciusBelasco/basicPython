dcb3886770329af50ff5eafd3d66dc0fddca6565
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-01 23:47:03 -0300

    Add exercise 6

---
+++ ex006.py
@@ -0,0 +1,6 @@
+number = int(input("Write a number: "))
+
+print("The number is {}".format(number))
+print("It's double is {}".format(number * 2))
+print("It's triple is {}".format(number * 3))
+print("It's square root is {:.3f}".format(number ** (1 / 2)))
