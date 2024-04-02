682f201bc456496bcf2b1b55adf792aeb1278d58
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-01 23:49:57 -0300

    Add functions to return predecessor and sucessor

--- ex005.py
+++ ex005.py
@@ -1,6 +1,17 @@
+def predecessor(number):
+
+    return number - 1
+
+
+def sucessor(number):
+
+    return number + 1
+
+
 n1 = int(input("Write a number: "))
+
 print(
     "The number is {}, the predecessor is {}, and de sucessor is {}!".format(
-        n1, n1 - 1, n1 + 1
+        n1, predecessor(n1), sucessor(n1)
     )
 )
