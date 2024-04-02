77940089d1d4732255fae9f0885472e0c1b208e6
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-01 23:53:56 -0300

    Change all calculus to functions

--- ex006.py
+++ ex006.py
@@ -1,6 +1,21 @@
+def double(number):
+
+    return number * 2
+
+
+def triple(number):
+
+    return number * 3
+
+
+def squareRootOf(number):
+
+    return number ** (1 / 2)
+
+
 number = int(input("Write a number: "))
 
 print("The number is {}".format(number))
-print("It's double is {}".format(number * 2))
-print("It's triple is {}".format(number * 3))
-print("It's square root is {:.3f}".format(number ** (1 / 2)))
+print("It's double is {}".format(double(number)))
+print("It's triple is {}".format(triple(number)))
+print("It's square root is {:.3f}".format(squareRootOf(number)))
