c6820e1d361ebed7aa7b9479af97d5c5180e2d6d
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-08 22:30:44 -0300

    Add exercise 13

---
+++ ex013.py
@@ -0,0 +1,8 @@
+# Do an Algorithm thar read the employ's salary and
+# show the new salary, with 15% of increase.
+
+salary = float(input("Actual wage: "))
+
+increase = salary * 0.15
+
+print("The new wage is {}!".format(salary + increase))
