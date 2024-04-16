3509d8abad9599b26541170af2859251b809ab63
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-15 23:06:32 -0300

    Change print and add exercise header

--- ex013.py
+++ ex013.py
@@ -5,4 +5,8 @@ salary = float(input("Actual wage: "))
 
 increase = salary * 0.15
 
-print("The new wage is {}!".format(salary + increase))
+print(
+    "Your actual wage is {:.2f},"
+    "and your wage with a rise of 15% "
+    "will be {:.2f}!".format(salary, salary + increase)
+)
--- ex014.py
+++ ex014.py
@@ -1,3 +1,6 @@
+# Write a program that convert an input temperature in
+# celcius to fahrenheit
+
 celcius = float(input("Insert the temperature in Celcius degrees: "))
 fahrenheit = (celcius - 32) * (5 / 9)
 
