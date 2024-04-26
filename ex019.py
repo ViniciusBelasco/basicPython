bdd580832140719a5cd0406c1dbd67c3dd48bc4e
ViniciusBelasco <vbrafaeli@gmail.com>
2024-04-25 23:56:33 -0300

    Add exercise 19

---
+++ ex019.py
@@ -0,0 +1,16 @@
+# A teacher want to draw one of your four studants to erease the
+# board. Do a program to help him, inputing his studants names and
+# printing the name of the chosen studant.
+
+import random
+
+number = int(input("How many studants will be in draw? "))
+studants = []
+
+for i in range(number):
+    studants.append(input("Insert a studant name "))
+    pass
+
+draw_studant = random.randint(1, len(studants))
+
+print('The selected studant is {}'.format(studants[draw_studant]))
