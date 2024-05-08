12907fa7f4619d02d86ae32a53e4cedb9832d021
ViniciusBelasco <vbrafaeli@gmail.com>
2024-05-08 00:02:00 -0300

    Add different ways to do the math

--- ex017.py
+++ ex017.py
@@ -4,9 +4,24 @@
 
 import math
 
-higth = float(input("Lenght of oposite side: "))
-widgth = float(input("Leght of adjacent leg: "))
+option = int(
+    input(
+        "please enter how you want to calculate the hipotenuse\n"
+        "basic math = 1 or math module 2: "
+    )
+)
 
-hipotenuse = math.sqrt(math.pow(higth, 2) + math.pow(widgth, 2))
+higth = float(input("Lenght of oposite side: "))
+width = float(input("Leght of adjacent leg: "))
+
+if option == 1:
+    hipotenuse = math.sqrt(math.pow(higth, 2) + math.pow(width, 2))
+
+elif option == 2:
+    hipotenuse = math.hypot(higth, width)
+
+else:
+    hipotenuse = (higth**2 + width**2) ** (1 / 2)
+
 
 print("The hipotenuse will measure: {:.2f}".format(hipotenuse))
