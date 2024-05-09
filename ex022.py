3c7413f147c5005594a2af6f254504697069710b
ViniciusBelasco <vbrafaeli@gmail.com>
2024-05-08 23:11:07 -0300

    Add exercise 22

---
+++ ex022.py
@@ -0,0 +1,19 @@
+# Create a program that input the full name of a person and show:
+# 1 - The name with all upper letters
+# 2 - The name with all lower letters
+# 3 - How many letters the string have (with no spaces)
+# 4 - How many letters the first name has
+
+fullName = str(input("Please enter your name: "))
+
+print(
+    "The full name in uppercase is {}. \n"
+    "In lowercase is {}. \n"
+    "It's lenght is {} \n"
+    "And the first name lenght is {}".format(
+        fullName.upper(),
+        fullName.lower(),
+        len(fullName),
+        len(fullName.split()[0])
+    )
+)
