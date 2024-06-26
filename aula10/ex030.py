843a4f63fee62d982fd026e39a6642608af8fc36
ViniciusBelasco <vbrafaeli@gmail.com>
2024-06-25 22:56:47 -0300

    Add exercise 30

---
+++ aula10/ex030.py
@@ -0,0 +1,9 @@
+# Create a program that reads an integer and show in the screen
+# if it is even or odd
+
+number = int(input("Write a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
