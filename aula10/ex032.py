93dae23e7e39ab46a8ed67e76503e742c591635f
ViniciusBelasco <vbrafaeli@gmail.com>
2024-07-01 09:08:16 -0300

    Add exercise 32 solution

--- aula10/ex032.py
+++ aula10/ex032.py
@@ -1,1 +1,8 @@
 # Create a program that reads a year and show if it is a leap year
+
+year = str(input("Which year do you wanna check? ")).strip().upper()
+
+if (int(year[2:4]) % 4) == 0:
+    print("It is a leap year")
+else:
+    print("It isn't a leap year")
